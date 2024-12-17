c1ed3f6bd06e43a2e775c83df10df445896b034d
Adam J. Jackson <a.j.jackson@physics.org>
2024-12-17 17:05:10 +0000

    Try going straight to build like Scipy does

--- recipe/build.sh
+++ recipe/build.sh
@@ -38,9 +38,12 @@ fi
 
 mkdir builddir
 
-# need to run meson first for cross-compilation case
-${PYTHON} $(which meson) setup ${MESON_ARGS} \
-    builddir || (cat builddir/meson-logs/meson-log.txt && exit 1)
+${PYTHON} -m build --wheel --no-isolation --skip-dependency-check -Cbuilddir=builddir  -Csetup-args=${MESON_ARGS// / -Csetup-args=} || (cat builddir/meson-logs/meson-log.txt && exit 1)
 
-${PYTHON} -m build --wheel --no-isolation --skip-dependency-check -Cbuilddir=builddir
+# # need to run meson first for cross-compilation case
+# ${PYTHON} $(which meson) setup ${MESON_ARGS} \
+#     builddir || (cat builddir/meson-logs/meson-log.txt && exit 1)
+
+# ${PYTHON} -m build --wheel --no-isolation --skip-dependency-check -Cbuilddir=builddir
+
 ${PYTHON} -m pip install dist/euphonic*.whl
