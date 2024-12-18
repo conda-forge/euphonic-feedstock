8795544b50680ec568f70b9f9a6c7478a654dfa2
Adam J. Jackson <a.j.jackson@physics.org>
2024-12-18 10:12:24 +0000

    Tidy up

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,13 +4,6 @@ set -ex
 
 # Perhaps we would not have embraced meson-python so quickly if we knew that.
 # Anyway, this seems to be how Scipy and Scikit-image work around it.
-#
-# Script is largely "borrowed" from scikit-image as we ran into the same build errors
-
-echo $(uname -s)
-
-# Remove problematic vsenv argument from pyproject.toml
-sed -i='.bkp' "s/setup = \[\'--vsenv\'\]/setup = []/" pyproject.toml
 
 # If linux, do regular meson-python build and exit early
 if test $(uname -s) = Linux
@@ -21,55 +14,41 @@ fi
 
 # Otherwise, it's a Mac
 
-# This variable is _probably_ harmless on Intel, but lets
-# avoid messing with that currently-working case
+# Remove problematic vsenv argument from pyproject.toml;
+# pip install with meson-python ignores it correctly but build will not
+sed -i='.bkp' "s/setup = \[\'--vsenv\'\]/setup = []/" pyproject.toml
+
+# Conda-forge uses cross-compilation for ARM:
+# - use pkg-config to find Numpy
+# - write a "cross file" (overwriting similar one from compilers)
 if [[ $target_platform == "osx-arm64" ]]
 then
     NUMPY_PKGCONFIG=$(find $BUILD_PREFIX -d -path '*numpy/_core/lib/pkgconfig')
     ls $NUMPY_PKGCONFIG
 
     export PKG_CONFIG_PATH=$BUILD_PREFIX/lib/pkgconfig:$NUMPY_PKGCONFIG
-    echo $PKG_CONFIG_PATH
 
-    $BUILD_PREFIX/bin/pkg-config --modversion numpy
-
-cat <<EOF > ${CONDA_PREFIX}/meson_cross_file.txt
+    cat <<EOF > ${CONDA_PREFIX}/meson_cross_file.txt
 [host_machine]
 system = 'darwin'
 cpu = 'arm64'
 cpu_family = 'aarch64'
 endian = 'little'
 [binaries]
-# cmake = '$BUILD_PREFIX/bin/cmake'
 pkg-config = '$BUILD_PREFIX/bin/pkg-config'
 [properties]
-needs_exe_wrapper = true
 python = '$BUILD_PREFIX/bin/python'
 
 EOF
 
 fi
 
-
-for PREFIX_ENV in $PREFIX $BUILD_PREFIX $CONDA_PREFIX
-do
-    echo "Searching prefix $PREFIX_ENV"
-    ls ${PREFIX_ENV}/bin
-    find $PREFIX_ENV -name 'numpy.pc'
-    find $PREFIX_ENV -name pkg-config
-done
-
-    echo ${PYTHON}
-    ${PYTHON} -c "import numpy"
-
+# There are some issues with pip install + meson-python +cross-compilation
+# so instead we build a wheel explicitly using the build package
 mkdir builddir
+${PYTHON} -m build --wheel --no-isolation --skip-dependency-check \
+          -Cbuilddir=builddir  -Csetup-args=${MESON_ARGS// / -Csetup-args=} \
+    || (cat builddir/meson-logs/meson-log.txt && exit 1)
 
-${PYTHON} -m build --wheel --no-isolation --skip-dependency-check -Cbuilddir=builddir  -Csetup-args=${MESON_ARGS// / -Csetup-args=} || (cat builddir/meson-logs/meson-log.txt && exit 1)
-
-# # need to run meson first for cross-compilation case
-# ${PYTHON} $(which meson) setup ${MESON_ARGS} \
-#     builddir || (cat builddir/meson-logs/meson-log.txt && exit 1)
-
-# ${PYTHON} -m build --wheel --no-isolation --skip-dependency-check -Cbuilddir=builddir
-
+# This wheel can then be safely installed
 ${PYTHON} -m pip install dist/euphonic*.whl
